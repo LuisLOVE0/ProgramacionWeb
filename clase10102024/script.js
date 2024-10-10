7a762faf65e903ff8866d68396be059ad6a48fd2
LuisLOVE0 <llopezv2200@alumno.ipn.mx>
2024-10-10 16:42:06 -0600

    arroz 20

---
+++ clase10102024/script.js
@@ -0,0 +1,9 @@
+//comentario de una linea
+/*comentarios 
+multilineas*/
+//declaraMOS NUESTRA FUNCION
+function hello(){
+    return 'oda mudzo'
+}
+console.log(hello());
+//tarea investigar funciones en javascript
