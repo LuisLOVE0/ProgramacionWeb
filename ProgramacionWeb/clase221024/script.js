5ee206d049b322d30f11b85498ba7275985a9b1a
LuisLOVE0 <lopezv2200@alumno.ipn.mx>
2024-10-22 17:46:34 -0600

    holas324

---
+++ ProgramacionWeb/clase221024/script.js
@@ -0,0 +1,17 @@
+function hola(a,b){
+    return a + b;
+}
+console.log(hola(5,4));
+
+const suma = (a,b) =>{
+    return a+b;
+}
+console.log(suma(8,5));
+
+const suma2 = (a,b) => a+b;
+console.log(suma2(2,8));
+
+
+const cuadrado = x => x*x;
+console.log(cuadrado(4)); 
+
